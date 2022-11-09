a110912335053fb5f583e4446b29769db9801c15
azerpas <xskylix@hotmail.fr>
2022-11-09 10:20:16 +0100

    test: test_apply_v2

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,11 +1,13 @@
 from discount_applier import DiscountApplier
+from typing import List, Dict
+import pytest
 
 class NotifierMock:
     def __init__(self):
         self.messages = []
 
     def notify(self, user, message):
-        self.messages.append(message)
+        self.messages.append({"user": user, "message": message})
 
 def test_apply_v1():
     # TODO: write a test that fails due to the bug in
@@ -21,4 +23,10 @@ def test_apply_v1():
 def test_apply_v2():
     # TODO: write a test that fails due to the bug in
     # DiscountApplier.apply_v2
+    notifier = NotifierMock()
+    applier = DiscountApplier(notifier)
+    list_of_users = ["user1", "user2"]
+    applier.apply_v2(20, list_of_users)
+    for i in range(0, len(list_of_users)):
+        assert list_of_users[i] in notifier.messages[i]['user'] 
     pass
